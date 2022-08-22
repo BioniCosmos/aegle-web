e5639b235bfcb40e58cab23b8be7e2879b6679a5
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2022-08-22 14:36:01 +0800

    Add the `WarningDialogBox` component
    Add a confirmation dialog before removing something to prevent mistaken
    operation.

---
+++ src/components/WarningDialogBox.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <div class="modal fade" id="warning-dialog-box" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Warning!</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Are you sure to remove <strong>{{ name }}</strong>?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="$emit('delete')">Yes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
