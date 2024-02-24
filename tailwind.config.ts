3a810748f59e138d091d18b0b2694d90d5f589d2
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2024-02-24 13:44:07 +0800

    Update the toolchain
    - Fix the type check issue.
    - Add ESLint.

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+    container: false,
+  },
+} satisfies Config
